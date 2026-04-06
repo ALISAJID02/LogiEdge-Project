938634f687f1f13fcb5f3e26e6d8e5024d098e69
ALISAJID <ssajid0004@gmail.com>
2026-04-06 15:53:52 +0530

    change db

--- server/db.js
+++ server/db.js
@@ -1,18 +1,16 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "logiedge",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
-  if (err) {
-    console.log("DB Error:", err);
-  } else {
-    console.log("MySQL Connected");
-  }
+  if (err) console.log(err);
+  else console.log("DB Connected 🚀");
 });
 
 module.exports = db;
